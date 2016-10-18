042a072d558e2d445ce140e487db790be4d4aabb
lorenzofantunes <fantunes.lorenzo@gmail.com>
2016-10-18 21:26:13 -0200

    normalize training

--- normalize0or1.py
+++ normalize0or1.py
@@ -5,7 +5,7 @@ import numpy as np
 print("Loading Data")
 
 #data = np.genfromtxt("mnist/mnist_train.csv", delimiter=',')
-data = np.genfromtxt("mnist/mnist_test.csv", delimiter=',')
+data = np.genfromtxt("mnist/mnist_train.csv", delimiter=',')
 
 print("Normalizing")
 for row in range(0, len(data)):
@@ -17,6 +17,6 @@ for row in range(0, len(data)):
 
 for row in data:
     #print(row)
-    with open("mnist/mnist_test0or1.csv", 'a') as mnist:
+    with open("mnist/mnist_train0or1.csv", 'a') as mnist:
         writer = csv.writer(mnist, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(row)
